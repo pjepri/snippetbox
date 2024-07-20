14a94985a3366254d3dd28bf247407bda2bf4de0
Peter Veshi <peterveshi@gmail.com>
2024-07-20 14:00:38 +0200

    wip: Middleware chapter, chaining middleware.

---
+++ cmd/web/routes.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+	"github.com/justinas/alice"
+)
+
+func (app *application) routes() http.Handler {
+	
+	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.snippetView)
+	mux.HandleFunc("/snippet/create", app.snippetCreate)
+
+	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+	return standard.Then(mux)
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module snippetbox.pjepri.net
+
+go 1.22.5
+
+require github.com/go-sql-driver/mysql v1.8.1
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/justinas/alice v1.2.0 // indirect
+)
